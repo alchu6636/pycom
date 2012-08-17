feb4cbfe4face4c5d29aed6358818cb5bc4ae0c0
alchu <alchu@swallow.(none)>
2012-08-18 06:41:03 +0900

    add testcase
    number ok
    add ok
    mul ok
    string error
    not yet float ok

--- test_pycom1.py
+++ test_pycom1.py
@@ -4,11 +4,26 @@ import unittest
 
 class TestPycom(unittest.TestCase):
 
-    def test_init(self):
+    def test_add(self):
         ast = simplecalc.simplecalc("2 + 3")
         self.assertEqual(ast.value, 5)
+
+    def test_mul(self):
+        ast = simplecalc.simplecalc("2*3")
+        self.assertEqual(ast.value, 6)
+
+    def test_string(self):
+        self.assertRaises(SystemExit, simplecalc.simplecalc, "a")
+
+    def test_num(self):
+        ast = simplecalc.simplecalc("1")
+        self.assertEqual(ast.value, 1)
+        ast = simplecalc.simplecalc("12")
+        self.assertEqual(ast.value, 12)
+
+    def test_float(self):
+        self.assertRaises(SystemExit, simplecalc.simplecalc, "1.5")
 
 if __name__ == '__main__':
     unittest.main()
 
-        
