e9836d3283fb6882cadd4050f294556e5dee29e2
alchu <alchu@swallow.(none)>
2012-08-18 06:49:00 +0900

    refactoring test_pycom1.py
    simple name by from,import simplecalc

--- test_pycom1.py
+++ test_pycom1.py
@@ -1,28 +1,28 @@
 
-import simplecalc
+from simplecalc import simplecalc
 import unittest
 
 class TestPycom(unittest.TestCase):
 
     def test_add(self):
-        ast = simplecalc.simplecalc("2 + 3")
+        ast = simplecalc("2 + 3")
         self.assertEqual(ast.value, 5)
 
     def test_mul(self):
-        ast = simplecalc.simplecalc("2*3")
+        ast = simplecalc("2*3")
         self.assertEqual(ast.value, 6)
 
     def test_string(self):
-        self.assertRaises(SystemExit, simplecalc.simplecalc, "a")
+        self.assertRaises(SystemExit, simplecalc, "a")
 
     def test_num(self):
-        ast = simplecalc.simplecalc("1")
+        ast = simplecalc("1")
         self.assertEqual(ast.value, 1)
-        ast = simplecalc.simplecalc("12")
+        ast = simplecalc("12")
         self.assertEqual(ast.value, 12)
 
     def test_float(self):
-        self.assertRaises(SystemExit, simplecalc.simplecalc, "1.5")
+        self.assertRaises(SystemExit, simplecalc, "1.5")
 
 if __name__ == '__main__':
     unittest.main()
