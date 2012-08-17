1b379be8d02d2491e305528c517a1dcb0d743e23
alchu <alchu@swallow.(none)>
2012-08-18 06:22:16 +0900

    modify test_pycom1.py
    now, auto test enable

--- test_pycom1.py
+++ test_pycom1.py
@@ -1,10 +1,14 @@
 
-import pycom
+import simplecalc
 import unittest
 
 class TestPycom(unittest.TestCase):
 
     def test_init(self):
-        Pycom()
+        ast = simplecalc.simplecalc("2 + 3")
+        self.assertEqual(ast.value, 5)
+
+if __name__ == '__main__':
+    unittest.main()
 
         
