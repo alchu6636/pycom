7d014f18e997362134727fa8ea00f37f9f7bd539
nakatani katsumi <alchu6636@gmail.com>
2012-08-18 10:17:23 +0900

    change test source code format
    change from double quote to single quote in test_simplecalc.py
    for test using string source code

--- test_simplecalc.py
+++ test_simplecalc.py
@@ -28,29 +28,29 @@ class TestPycom(unittest.TestCase):
         return ast.value
 
     def test_num(self):
-        self.assertEqual(self.calc("1"), 1)
-        self.assertEqual(self.calc("12"), 12)
+        self.assertEqual(self.calc('1'), 1)
+        self.assertEqual(self.calc('12'), 12)
 
     def test_add(self):
-        self.assertEqual(self.calc("2 + 3"), 5)
+        self.assertEqual(self.calc('2 + 3'), 5)
 
     def test_mul(self):
-        self.assertEqual(self.calc("2 * 3"), 6)
+        self.assertEqual(self.calc('2 * 3'), 6)
 
     def test_space(self):
-        self.assertEqual(self.calc("1+2"), 3)
-        self.assertEqual(self.calc("1 + 2"), 3)
-        self.assertEqual(self.calc("  1  +  2  "), 3)
+        self.assertEqual(self.calc('1+2'), 3)
+        self.assertEqual(self.calc('1 + 2'), 3)
+        self.assertEqual(self.calc('  1  +  2  '), 3)
         
     def test_null(self):
-        self.assertRaises(SystemExit, self.calc, "")
-        self.assertRaises(SystemExit, self.calc, " ")
+        self.assertRaises(SystemExit, self.calc, '')
+        self.assertRaises(SystemExit, self.calc, ' ')
 
     def test_string(self):
-         self.assertRaises(SystemExit, self.calc, "a")
+         self.assertRaises(SystemExit, self.calc, 'a')
 
     def test_float(self):
-        self.assertRaises(SystemExit, self.calc, "1.5")
+        self.assertRaises(SystemExit, self.calc, '1.5')
 
 if __name__ == '__main__':
     unittest.main()
