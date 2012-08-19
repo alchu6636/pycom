9ab5c3d8d48124887090bd192e2af88114c5a8da
nakatani katsumi <alchu6636@gmail.com>
2012-08-19 18:24:21 +0900

    add test and refactoring
    add tests for PLY base code
    add test for binary operation
    add function for multi lines source

--- test_classcalc.py
+++ test_classcalc.py
@@ -6,24 +6,33 @@ def calc(s):
     calc.run_string(s)
     return calc.result[-1]
     
+def calcm(s):
+    slist = s.split('\n')
+    calc = classcalc.Calc()
+    for line in slist:
+        calc.run_string(line)
+    return calc.result
+
 class TestCalc(unittest.TestCase):
         
-    def test_num(self):
+    def test_int(self):
         self.assertEqual(calc('3'), 3)
         
-    def test_add(self):
-        self.assertEqual(calc('3 + 23'), 26)
-
-    def test_lines(self):
-        calc = classcalc.Calc()
-        calc.run_string('3')
-        calc.run_string('2')
-        self.assertEqual(calc.result[0], 3)
-        self.assertEqual(calc.result[1], 2)
+    def test_float(self):
+        self.assertRaises(IndexError, calc, '3.2')
         
-        
-#    def test_var(self):
-#        self.assertEqual(calc('a=3'), 7)
+    def test_binop(self):
+        data = [('1+2', 3),
+                ('2*3', 6),
+                ('5-2', 3),
+                ('7/3', 2)
+                ]
+        for p in data:
+            self.assertEquals(calc(p[0]), p[1], p[0]+' != '+str(p[1]))
 
+    def test_variable(self):
+        r = calcm('a= 3 * 2\n a-1')
+        self.assertEqual(r[-1], 5)
+        
 if __name__ == '__main__':
     unittest.main()
