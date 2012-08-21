ae35272411b021d75ed5a216db7cbe31976f45d9
alchu <alchu@swallow.(none)>
2012-08-22 05:44:30 +0900

    add tests for syntax  expr.method(expr)
    add tests folloing
    float add int
    float mul float
    string add string
    string mul int

--- test_classcalc.py
+++ test_classcalc.py
@@ -26,10 +26,22 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc('"abc"'), 'abc')
 
     def test_int_add_int(self):
-        self.assertEqual(calc('3 .add(0)'), 3)
+        self.assertEqual(calc('3 .add(0)'), 3) # need space flollow '3'
 
     def test_int_mul_int(self):
         self.assertEqual(calc('4 .mul(1)'), 4)
+
+    def test_float_add_int(self):
+        self.assertEqual(calc('1.25 .add(0)'), 1.25)
+
+    def test_float_mul_int(self):
+        self.assertEqual(calc('1.25 .mul(1)'), 1.25)
+
+    def test_str_add_str(self):
+        self.assertEqual(calc('"abc".add("")'), 'abc')
+
+    def test_str_mul_int(self):
+        self.assertEqual(calc('"abc".mul(1)'), 'abc')
 
 if __name__ == '__main__':
     unittest.main()
