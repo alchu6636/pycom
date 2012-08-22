726dc868f44391d5a5825cf18d6ccfb39c6635ef
Nakatani Katsumi <alchu6636@gmail.com>
2012-08-23 06:25:13 +0900

    add test
    add more pattern test

--- test_classcalc.py
+++ test_classcalc.py
@@ -27,21 +27,27 @@ class TestCalc(unittest.TestCase):
 
     def test_int_add_int(self):
         self.assertEqual(calc('3 .add(0)'), 3) # need space following int
+        self.assertEqual(calc('3 .add(1)'), 4) # need space following int
 
     def test_int_mul_int(self):
         self.assertEqual(calc('4 .mul(1)'), 4)
+        self.assertEqual(calc('4 .mul(2)'), 8)
 
     def test_float_add_int(self):
         self.assertEqual(calc('1.25.add(0)'), 1.25)
+        self.assertEqual(calc('1.25.add(1)'), 2.25)
 
     def test_float_mul_int(self):
         self.assertEqual(calc('1.25.mul(1)'), 1.25)
+        self.assertEqual(calc('1.25.mul(2)'), 2.5)
 
     def test_str_add_str(self):
         self.assertEqual(calc('"abc".add("")'), 'abc')
+        self.assertEqual(calc('"abc".add("def")'), 'abcdef')
 
     def test_str_mul_int(self):
         self.assertEqual(calc('"abc".mul(1)'), 'abc')
+        self.assertEqual(calc('"abc".mul(2)'), 'abcabc')
 
 if __name__ == '__main__':
     unittest.main()
